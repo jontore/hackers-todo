1374f8fc09723ae584a0b9e68f9f7bfb077661cb
Stig Murberg <stimurbe@cisco.com>
2012-11-15 22:32:26 +0100

    more backbone goodness

--- src/todo.js
+++ src/todo.js
@@ -1,12 +1,17 @@
 $(function() {
 
-  var Task = Backbone.Model.extend({});
+  var Task = Backbone.Model.extend({
+    defaults: {
+      desc: '',
+      done: false
+    }
+  });
 
   var TaskList = Backbone.Collection.extend({
     model: Task
   });
 
-  var AddTaskView = Backbone.View.extend({
+  var NewTodo = Backbone.View.extend({
 
     el: '#add-task',
 
@@ -26,14 +31,15 @@ $(function() {
 
     onKeyPress: function(e) {
       if (e.keyCode == 13 && this.input.val()) {
-        var frag = "<li><a>" + this.input.val() + "</a></li>";
-        $('#todo-list ul').append(frag);
+        this.tasks.add({
+          desc: this.input.val()
+        });
         this.input.val('');
-      }      
+      }
     }
   });
 
-  var TaskView = Backbone.View.extend({
+  var TodoList = Backbone.View.extend({
 
     el: '#todo-list',
 
@@ -43,22 +49,57 @@ $(function() {
 
     initialize: function(tasks) {
       this.tasks = tasks;
+      tasks.on('add', _.bind(this.onTaskAdded, this));
+      tasks.on('change', _.bind(this.onTaskCompleted, this));
     },
 
     render: function() {
-      this.$el.append('<ul class="nav nav-tabs nav-stacked" data-toggle="tabs"></ul>');
+      this.ul = $('<ul class="nav nav-tabs nav-stacked" data-toggle="tabs"></ul>')
+      this.$el.append(this.ul);
+    },
+
+    onTaskAdded: function(task) {
+      var taskEl = $('<li><a data-task-id="' + task.cid + '">' + task.get('desc') + '</a></li>');
+      this.ul.append(taskEl);
+    },
+
+    onTaskCompleted: function(task) {
+      var taskEl = this.$el.find('[data-task-id=' + task.cid + ']').parents('li');
+      taskEl.slideUp('slow', function() {
+        taskEl.remove();
+      });
     },
 
     onClick: function(e) {
-      $(e.target).parents('li').remove();
+      var cid = $(e.target).attr('data-task-id');
+      this.tasks.getByCid(cid).set('done', true);
+    }
+
+  });
+
+  var DoneList = Backbone.View.extend({
+
+    el: '#done-list',
+
+    initialize: function(tasks) {
+      tasks.on('change', _.bind(this.onTaskCompleted, this));
+    },
+
+    render: function() {
+      this.ul = $('<ul class="nav nav-tabs nav-stacked" data-toggle="tabs" style="opacity: 0.3"></ul>');
+      this.$el.append(this.ul);
+    },
+
+    onTaskCompleted: function(task) {
+      this.ul.append('<li><a data-task-id="' + task.cid + '">' + task.get('desc') + '</a></li>');
     }
 
   });
 
   var tasks = new TaskList();
 
-  new TaskView(tasks).render();
-  
-  new AddTaskView(tasks).render();
+  new TodoList(tasks).render();
+  new DoneList(tasks).render();
+  new NewTodo(tasks).render();
 
 });
