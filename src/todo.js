f8093a1d61aefde8ea7e52b285b9f415cfc34f12
Stig Murberg <stimurbe@cisco.com>
2012-11-15 20:49:48 +0100

    started on the backbone version

--- src/todo.js
+++ src/todo.js
@@ -1,18 +1,64 @@
 $(function() {
 
-  function addTask(e) {
-    if (e.keyCode == 13 && $(this).val()) {
-      var frag = "<li><a>" + $(this).val() + "</a></li>";
-      $('#todo-list').append(frag);
-      $(this).val('');
+  var Task = Backbone.Model.extend({});
+
+  var TaskList = Backbone.Collection.extend({
+    model: Task
+  });
+
+  var AddTaskView = Backbone.View.extend({
+
+    el: '#add-task',
+
+    events: {
+      'keypress': 'onKeyPress'
+    },
+
+    initialize: function(tasks) {
+      this.tasks = tasks;
+    },
+
+    render: function() {
+      this.input = $('<input id="add-task" type="text" placeholder="Add task" />');
+      this.$el.append(this.input);
+      this.input.focus();
+    },
+
+    onKeyPress: function(e) {
+      if (e.keyCode == 13 && this.input.val()) {
+        var frag = "<li><a>" + this.input.val() + "</a></li>";
+        $('#todo-list ul').append(frag);
+        this.input.val('');
+      }      
     }
-  }
+  });
 
-  function removeTask(e) {
-    $(e.target).parents('li').remove();
-  }
+  var TaskView = Backbone.View.extend({
 
-  $('#add-task').keypress(addTask).focus();
-  $('#todo-list').click(removeTask);
+    el: '#todo-list',
+
+    events: {
+      'click': 'onClick'
+    },
+
+    initialize: function(tasks) {
+      this.tasks = tasks;
+    },
+
+    render: function() {
+      this.$el.append('<ul class="nav nav-tabs nav-stacked" data-toggle="tabs"></ul>');
+    },
+
+    onClick: function(e) {
+      $(e.target).parents('li').remove();
+    }
+
+  });
+
+  var tasks = new TaskList();
+
+  new TaskView(tasks).render();
+  
+  new AddTaskView(tasks).render();
 
 });
